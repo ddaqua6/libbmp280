63c2d0d87159316a2f41e5d1fa0c6443097a1a6d
ddaqua6 <76646834+ddaqua6@users.noreply.github.com>
2021-11-27 16:05:31 +0100

    Create libbmp280.py

---
+++ libbmp280.py
@@ -0,0 +1,32 @@
+import time
+from bmp280 import BMP280
+
+temperature_calibration = -0.77
+pressure_calibration = 22
+
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+
+# Initialise the BMP280
+bus = SMBus(1)
+bmp280 = BMP280(i2c_dev=bus)
+
+def zaokrouhlit(val, dec):
+    if (dec != False):
+        return "{:.2f}".format(val)
+    else:
+        return val
+
+def temperature(decimals=False):
+    return zaokrouhlit(bmp280.get_temperature() + temperature_calibration, decimals)
+def pressure(decimals=False):
+    return zaokrouhlit(bmp280.get_pressure() + pressure_calibration, decimals)
+def raw_temperature(decimals=False):
+    return zaokrouhlit(bmp280.get_temperature(), decimals)
+def raw_pressure(decimals=False):
+    return zaokrouhlit(bmp280.get_pressure(), decimals)
+
+prvni_teplota = raw_temperature()
+prvni_tlak = raw_pressure()
